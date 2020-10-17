0e9f597c00b43b5651f8394bbce1e9af531c2662
Teddy Kekana <teddykekana@Teddys-MacBook-Air.local>
2020-10-17 23:49:39 +0200

    SSH into EC2 instance from AWS Lambda

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const fs = require("fs");
+const fs = require('fs');
 
 const uploadFileToSftpServer = async (content, remoteFolder) => {
       try {
@@ -10,8 +10,8 @@ const uploadFileToSftpServer = async (content, remoteFolder) => {
             username: 'ubuntu',
             privateKey: fs.readFileSync('./path/to/your/key.pem'),
           };
-          const Client = require("ssh2-sftp-client");
-          const { Parser } = require("json2csv");
+          const Client = require('ssh2-sftp-client');
+          const { Parser } = require('json2csv');
           const sftp = new Client();
           // Extract fields' headers from a JSON object / file
           const fields = Object.keys(content[0]);
@@ -46,32 +46,32 @@ module.exports.sftpFileUpload = async (event, context) => {
   // Sample JSON data
   const data = [
     {
-      color: "red",
-      value: "#f00"
+      color: 'red',
+      value: '#f00'
     },
     {
-      color: "green",
-      value: "#0f0"
+      color: 'green',
+      value: '#0f0'
     },
     {
-      color: "blue",
-      value: "#00f"
+      color: 'blue',
+      value: '#00f'
     },
     {
-      color: "cyan",
-      value: "#0ff"
+      color: 'cyan',
+      value: '#0ff'
     },
     {
-      color: "magenta",
-      value: "#f0f"
+      color: 'magenta',
+      value: '#f0f'
     },
     {
-      color: "yellow",
-      value: "#ff0"
+      color: 'yellow',
+      value: '#ff0'
     },
     {
-      color: "black",
-      value: "#000"
+      color: 'black',
+      value: '#000'
     }
   ];
   const res = await uploadFileToSftpServer(data, 'Uploads/');
